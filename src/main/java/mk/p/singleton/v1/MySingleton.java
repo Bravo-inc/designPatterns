e55be7c60bccd4e5a8a7b78c25fff1256f1a0e9c
Max Kovalenko <max.kovalenko.zp@gmail.com>
2019-01-18 13:20:14 +0200

    C3

--- src/main/java/mk/p/singleton/v1/MySingleton.java
+++ src/main/java/mk/p/singleton/v1/MySingleton.java
@@ -5,9 +5,9 @@ public class MySingleton {
         System.out.println("Singleton created");
     }
 
-    public static final MySingleton INSTANCE = new MySingleton();
-
     public void printName() {
         System.out.println("I am a Singleton");
     }
+
+    public static final MySingleton INSTANCE = new MySingleton();
 }
